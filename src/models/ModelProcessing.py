f3ed70bf1eb62df3dd04159937c94e69d31273ad
Matheus Henrique C.T. Souza <149204617+MHCTDS@users.noreply.github.com>
2025-07-11 18:18:12 -0300

    Update ModelProcessing with CV K-Fold

--- src/models/ModelProcessing.py
+++ src/models/ModelProcessing.py
@@ -3,118 +3,97 @@ from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import recall_score
-from sklearn.metrics import precision_score
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import f1_score
+from sklearn.model_selection import cross_validate, StratifiedKFold
 from sklearn.dummy import DummyClassifier
 
 
 # Classe de treinamento dos modelos e metricas não otimizadas/todas as iterações
 class ModelProcessing:
-    def dummy_model(train_data, train_target, test_data, test_target):
-        model = DummyClassifier(strategy="most_frequent")
-        model.fit(train_data.toarray(), train_target)
-        prediction = model.predict(test_data)
-        recall = recall_score(test_target, prediction, average="weighted")
-        acc = accuracy_score(test_target, prediction)
-        f1 = f1_score(test_target, prediction, average="weighted")
-        precision = precision_score(test_target, prediction, average="weighted")
-        print("best recall:", recall)
-        print("best accuracy:", acc)
-        print("best f1:", f1)
-        print("best precision:", precision)
+    def dummy_model(data,target):
+        kf=StratifiedKFold(n_splits=5,shuffle=True,random_state=12345)
+        model=DummyClassifier(strategy="most_frequent")
+        scoring = ['accuracy','precision_weighted', 'recall_weighted','f1_weighted']
+        scores = cross_validate(model, data, target,cv=kf, scoring=scoring)
+        score_values=[score.mean() for score in scores.values()]
+        print(sorted(scores.keys()))
+        print([score.mean() for score in scores.values()])
+        print('best recall:',score_values[4])
+        print('best accuracy:',score_values[2])
+        print('best f1:',score_values[5])
+        print('best precision:',score_values[3])
         return model
-
-    def adaboost_model(train_data, train_target, test_data, test_target):
-        best_recall = 0
-        # Talvez não tenha parametro  ou random_state
-        # Talvez tenha parametro learning rate, talvez vale a pena testar
-        for estimators in tqdm(range(1, 100)):
-            for depth in range(1, 10):
-                model = AdaBoostClassifier(n_estimators=estimators, random_state=12345)
-                model.fit(train_data, train_target)
-                prediction = model.predict(test_data)
-                recall = recall_score(test_target, prediction, average="weighted")
-                if best_recall < recall:
-                    best_model = model
-                    best_depth = depth
-                    best_recall = recall
-                    best_estimators = estimators
-                    best_acc = accuracy_score(test_target, prediction)
-                    best_f1 = f1_score(test_target, prediction, average="weighted")
-                    best_precision = precision_score(
-                        test_target, prediction, average="weighted", zero_division=0
-                    )
-        print("best depth:", best_depth)
-        print("best number of estimators:", best_estimators)
-        print("best recall:", best_recall)
-        print("best accuracy:", best_acc)
-        print("best f1:", best_f1)
-        print("best precision:", best_precision)
+    def adaboost_model(data,target):
+        kf=StratifiedKFold(n_splits=5,shuffle=True,random_state=12345)
+        best_score_values=[0,0,0,0,0,0]
+        #Talvez não tenha parametro  ou random_state
+        #Talvez tenha parametro learning rate, talvez vale a pena testar
+        for estimators in tqdm(range(1,100)):
+            model = AdaBoostClassifier(n_estimators=estimators, random_state=12345)
+            scoring = ['accuracy','precision_weighted', 'recall_weighted','f1_weighted']
+            scores = cross_validate(model, data, target,cv=kf, scoring=scoring)
+            scores_values=[score.mean() for score in scores.values()]
+            if best_score_values[4]<scores_values[4]:
+                best_model=model
+                best_estimators=estimators
+                best_score=scores
+                best_score_values=scores_values
+        print('best number of estimators:',best_estimators)
+        print('best recall:',best_score_values[4])
+        print('best accuracy:',best_score_values[2])
+        print('best f1:',best_score_values[5])
+        print('best precision:',best_score_values[3])
         return best_model
-
-    def florest_model(train_data, train_target, test_data, test_target):
-        best_recall = 0
-        for size in tqdm(range(1, 50)):
-            for depth in range(1, 30):
-                model = RandomForestClassifier(
-                    random_state=123456789, max_depth=depth, n_estimators=size
-                )
-                model.fit(train_data, train_target)
-                prediction = model.predict(test_data)
-                recall = recall_score(test_target, prediction, average="weighted")
-                if best_recall < recall:
-                    best_model = model
-                    best_depth = depth
-                    best_recall = recall
-                    best_size = size
-                    best_acc = accuracy_score(test_target, prediction)
-                    best_f1 = f1_score(test_target, prediction, average="weighted")
-                    best_precision = precision_score(
-                        test_target, prediction, average="weighted", zero_division=0
-                    )
-        print("best depth:", best_depth)
-        print("best size:", best_size)
-        print("best recall:", best_recall)
-        print("best accuracy:", best_acc)
-        print("best f1:", best_f1)
-        print("best precision:", best_precision)
+    def florest_model(data,target):
+        kf=StratifiedKFold(n_splits=5,shuffle=True,random_state=12345)
+        best_score_values=[0,0,0,0,0,0]
+        for size in tqdm(range(1,50)):
+            for depth in range(1,30):
+                model=RandomForestClassifier(random_state=123456789,max_depth=depth,n_estimators=size)
+                scoring = ['accuracy','precision_weighted', 'recall_weighted','f1_weighted']
+                scores = cross_validate(model, data, target,cv=kf, scoring=scoring)
+                scores_values=[score.mean() for score in scores.values()]
+                if best_score_values[4]<scores_values[4]:
+                    best_model=model
+                    best_size=size
+                    best_depth=depth
+                    best_score=scores
+                    best_score_values=scores_values
+        print('best depth:',best_depth)
+        print('best size:',best_size)
+        print('best recall:',best_score_values[4])
+        print('best accuracy:',best_score_values[2])
+        print('best f1:',best_score_values[5])
+        print('best precision:',best_score_values[3])
         return best_model
-
-    def tree_model(train_data, train_target, test_data, test_target):
-        best_recall = 0
-        for depth in tqdm(range(1, 100)):
-            model = DecisionTreeClassifier(random_state=123456789, max_depth=depth)
-            model.fit(train_data, train_target)
-            prediction = model.predict(test_data)
-            recall = recall_score(test_target, prediction, average="weighted")
-            if best_recall < recall:
-                best_model = model
-                best_depth = depth
-                best_recall = recall
-                best_acc = accuracy_score(test_target, prediction)
-                best_f1 = f1_score(test_target, prediction, average="weighted")
-                best_precision = precision_score(
-                    test_target, prediction, average="weighted", zero_division=0
-                )
-        print("best depth:", best_depth)
-        print("best recall:", best_recall)
-        print("best accuracy:", best_acc)
-        print("best f1:", best_f1)
-        print("best precision:", best_precision)
+    def tree_model(data,target):
+        kf=StratifiedKFold(n_splits=5,shuffle=True,random_state=12345)
+        best_score_values=[0,0,0,0,0,0]
+        for depth in tqdm(range(1,100)):
+            model=DecisionTreeClassifier(random_state=123456789,max_depth=depth)
+            scoring = ['accuracy','precision_weighted', 'recall_weighted','f1_weighted']
+            scores = cross_validate(model, data, target,cv=kf, scoring=scoring)
+            scores_values=[score.mean() for score in scores.values()]
+            if best_score_values[4]<scores_values[4]:
+                best_model=model
+                best_depth=depth
+                best_score=scores
+                best_score_values=scores_values
+        print('best depth:',best_depth)
+        print('best recall:',best_score_values[4])
+        print('best accuracy:',best_score_values[2])
+        print('best f1:',best_score_values[5])
+        print('best precision:',best_score_values[3])
         return best_model
-
-    def naivebayes_model(train_data, train_target, test_data, test_target):
+    def naivebayes_model(data,target):
+        kf=StratifiedKFold(n_splits=5,shuffle=True,random_state=12345)
         model = GaussianNB()
-        model.fit(train_data.toarray(), train_target)
-        prediction = model.predict(test_data.toarray())
-        recall = recall_score(test_target, prediction, average="weighted")
-        acc = accuracy_score(test_target, prediction)
-        f1 = f1_score(test_target, prediction, average="weighted")
-        precision = precision_score(test_target, prediction, average="weighted")
-        print("best recall:", recall)
-        print("best accuracy:", acc)
-        print("best f1:", f1)
-        print("best precision:", precision)
+        scoring = ['accuracy','precision_weighted', 'recall_weighted','f1_weighted']
+        scores = cross_validate(model, data.toarray(), target,cv=kf, scoring=scoring)
+        score_values=[score.mean() for score in scores.values()]
+        print(sorted(scores.keys()))
+        print([score.mean() for score in scores.values()])
+        print('best recall:',score_values[4])
+        print('best accuracy:',score_values[2])
+        print('best f1:',score_values[5])
+        print('best precision:',score_values[3])
         return model
